b770c365d84be0a83353517cb8e61c6d0b5a9faa
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-04-11 15:19:11 +0300

    Config relative paths for icons and components

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,12 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@icons': fileURLToPath(new URL('./src/components/icons/index.ts', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components/index.ts', import.meta.url))
     }
   }
 })
